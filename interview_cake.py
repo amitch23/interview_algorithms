de0dbd6f0e7c508a25a1a610da865a1369c4d1a5
amitch23 <andrealeemitchell@gmail.com>
2015-05-07 12:18:42 -0700

    added interview cake problem

---
+++ interview_cake.py
@@ -0,0 +1,63 @@
+"""
+You've built an in-flight entertainment system
+with on-demand movie streaming.
+
+Users on longer flights like to start a second movie
+ right when their first one ends, but they complain
+ that the plane usually lands before they can see the ending.
+ So you're building a feature for choosing two movies
+ whose total runtimes will equal the exact flight length.
+
+Write a function that takes an integer flight_length
+ (in minutes) and an array of integers movie_lengths
+ (in minutes) and returns a boolean indicating whether
+  there are two numbers in movie_lengths whose sum equals
+  flight_length.
+
+When building your function:
+
+-Assume your users will watch exactly two movies
+-Don't make your users watch the same movie twice
+-Optimize for runtime over memory
+
+"""
+
+# Tackling the pressing problems of our day and age.
+
+flight_length = 180
+movie_lengths = [60, 90, 120, 63, 100, 90]
+
+
+def can_watch_two_movies(flight_length, movie_lengths):
+
+    #poor runtime: nested for loop, check each sum
+    movie_sums=[]
+
+    for movie in movie_lengths:
+        for m in range(len(movie_lengths)/2):
+
+            if (movie_lengths[m] + movie == flight_length) and (movie_lengths[m] != movie):
+                movie_sums.append((movie_lengths[m], movie))
+            else:
+                continue
+
+    if movie_sums:
+        return True
+    else:
+        return False
+
+def can_watch_two_movies_faster(flight_length, movie_lengths):
+
+    #put movie_lengths in a dictionary:
+    {'1': 90, '2':97, '3': 120}
+
+    {'90': 90, '97': 97, '120': 120}
+
+    #some type of sort method?
+
+
+
+
+
+
+
